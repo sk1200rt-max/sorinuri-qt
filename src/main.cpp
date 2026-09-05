075862a115762d789b03fb001d6b79f28f625336
sk1200rt-max <sk1200rt@gmail.com>
2026-09-05 17:55:54 +0000

    fix: refresh originals catalog from canonical feed

--- src/main.cpp
+++ src/main.cpp
@@ -97,7 +97,7 @@ int main(int argc, char* argv[])
     QApplication app(argc, argv);
     app.setApplicationName("Sorinuri");
     app.setApplicationDisplayName("소리누리");
-    app.setApplicationVersion("6.21.7");
+    app.setApplicationVersion("6.21.8");
     app.setOrganizationName("Sorinuri");
     app.setWindowIcon(QIcon(":/icons/sorinuri.ico"));
 
